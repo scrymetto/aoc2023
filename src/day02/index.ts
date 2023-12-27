b00409140e02a7a70f5ad3a4f99bb87d73a33566
Olya Mityukhina <olga.mityukhina@semrush.com>
2023-12-27 16:15:28 +0700

    second day second part

--- src/day02/index.ts
+++ src/day02/index.ts
@@ -8,9 +8,8 @@ const MAX_GREEN = 13;
 const MAX_RED = 12;
 const MAX_BLUE = 14;
 
-const part1 = (rawInput: string) => {
-  const input = parseInput(rawInput);
-  const games = input
+const getGames = (input: string): [string, SetType[]] => {
+  return input
     .split("Game ")
     .map(val => val.split(": "))
     .map(val => {
@@ -35,9 +34,13 @@ const part1 = (rawInput: string) => {
           return acc;
         }, []);
       return [Number(val[0]), sets];
-    });
+    }) as [string, SetType[]];
+};
 
-  return games.reduce((acc, game) => {
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  return getGames(input).reduce((acc, game) => {
     let isPossible = true;
     if (game.length === 0) return acc;
     // @ts-ignore
@@ -48,13 +51,26 @@ const part1 = (rawInput: string) => {
     });
 
     return isPossible ? acc + (game[0] as number) : acc;
-  }, 0);
+  }, 0)
 };
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
-
-  return;
+  return getGames(input).reduce((acc, game) => {
+    const max: SetType = {
+      red: 0,
+      blue: 0,
+      green: 0,
+    };
+    if (game.length === 0) return acc;
+    // @ts-ignore
+    game[1]?.forEach(set => {
+      if (set["green"] > (max.green || 0)) max.green = set["green"];
+      if (set["blue"] > (max.blue || 0)) max.blue = set["blue"];
+      if (set["red"] > (max.red || 0)) max.red = set["red"];
+    });
+    return acc + (max.blue! * max.red! * max.green!);
+  }, 0);
 };
 
 run({
@@ -74,10 +90,15 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+`,
+        expected: 2286,
+      },
     ],
     solution: part2,
   },
